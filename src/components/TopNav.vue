33e3dff76519a6e8909ef7c77ac8911b7659bf0a
12am <121148062+12amtop@users.noreply.github.com>
2023-03-09 18:46:33 +0800

    实现toggle切换aside

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,6 +1,14 @@
+<script setup lang="ts">
+import { Ref, inject } from 'vue';
+const asideVisible = inject<Ref<boolean>>('xxx')
+const toggleAside = ()=>{
+    asideVisible.value = !asideVisible.value
+}
+</script>
 <template>
     <div class="topNav">
-        <div class="logo">LOGO</div>
+        <span class="menuChangeButton" @click="toggleAside">=</span>
+        <div class="logo" >LOGO</div>
         <ul class="menu">
             <li>菜单1</li>
             <li>菜单2</li>
@@ -15,8 +23,14 @@
     justify-content: space-between;
     align-items: center;
     height: 4rem;
+    position: relative;
+    z-index: 0;
+    .menuChangeButton {
+        color: red;
+        position: absolute;
+    }
     .logo {
-        margin-left: 1rem;
+        margin-left: 1.5rem;
     }
 
     .menu {
@@ -29,4 +43,5 @@
             margin-left: 1rem;
         }
     }
-}</style>
+}
+</style>
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
+import { Ref, inject } from 'vue';
 import TopNav from '../components/TopNav.vue'
+const asideVisible = inject<Ref<boolean>>('xxx')
 </script>
 
 <template>
     <TopNav></TopNav>
     <div class="content">
-        <aside>
+        <aside v-if="asideVisible">
             <h2>组件列表</h2>
             <ol>
                 <li>
@@ -28,10 +30,13 @@ import TopNav from '../components/TopNav.vue'
 
 <style lang="scss" scoped>
     .content {
+        line-height: 2.5rem;
         background-color: gray;
-        width: 20%;
+        width: 25%;
         position: fixed;
         top: 0;
         left: 0;
+        z-index: -1;
+        padding-top: 5.5rem;
     }
 </style>
